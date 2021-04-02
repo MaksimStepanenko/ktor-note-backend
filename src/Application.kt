33d25ae1229b294ed877f3efe888cc732b608919
Maksim Stepanenko <kreeper@ip13.ru>
2021-04-02 14:42:14 +0300

    add structure

--- src/Application.kt
+++ src/Application.kt
@@ -1,13 +1,24 @@
 package com.example
 
+import com.fasterxml.jackson.core.util.DefaultIndenter
+import com.fasterxml.jackson.core.util.DefaultPrettyPrinter
+import com.fasterxml.jackson.databind.SerializationFeature
 import io.ktor.application.*
-import io.ktor.response.*
-import io.ktor.request.*
-import io.ktor.routing.*
-import io.ktor.http.*
-import com.fasterxml.jackson.databind.*
-import io.ktor.jackson.*
 import io.ktor.features.*
+import io.ktor.http.*
+import io.ktor.jackson.*
+import io.ktor.response.*
+import io.ktor.routing.*
+
+val lastId: Int = 1
+val notes = mutableMapOf<Int, String>()
+
+data class HttpBinError(
+    val request: String,
+    val message: String,
+    val code: HttpStatusCode,
+    val cause: Throwable? = null
+)
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
@@ -16,24 +27,65 @@ fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 fun Application.module(testing: Boolean = false) {
     install(ContentNegotiation) {
         jackson {
-            enable(SerializationFeature.INDENT_OUTPUT)
+            configure(SerializationFeature.INDENT_OUTPUT, true)
+            setDefaultPrettyPrinter(DefaultPrettyPrinter().apply {
+                indentArraysWith(DefaultPrettyPrinter.FixedSpaceIndenter.instance)
+                indentObjectsWith(DefaultIndenter("  ", "\n"))
+            })
         }
     }
 
-    install(AutoHeadResponse)
-
-    install(DefaultHeaders) {
-        header("X-Engine", "Ktor") // will send this header with each response
+    install(StatusPages) {
+        exception<NumberFormatException> {
+            val error = HttpBinError(
+                code = HttpStatusCode.BadRequest,
+                request = call.request.local.uri,
+                message = "id param must be integer"
+            )
+            call.respond(error)
+        }
     }
 
     routing {
-        get("/") {
-            call.respondText("HELLO WORLD!", contentType = ContentType.Text.Plain)
+
+        route("/notes") {
+            get {
+                call.respond(notes)
+            }
+            post {
+                call.request
+            }
         }
 
-        get("/json/jackson") {
-            call.respond(mapOf("hello" to "world"))
+        route("/notes/{id}") {
+
+            get {
+                val id = call.parameters["id"]?.toInt()
+
+            }
+
+            put {
+                val id = call.parameters["id"]?.toInt()
+
+            }
+
+            delete {
+                call.respond(status = HttpStatusCode.OK, "")
+            }
+
+        }
+
+        route("{...}") {
+            handle {
+                val error = HttpBinError(
+                    code = HttpStatusCode.NotFound,
+                    request = call.request.local.uri,
+                    message = "NOT FOUND"
+                )
+                call.respond(error)
+            }
         }
     }
+
 }
 
